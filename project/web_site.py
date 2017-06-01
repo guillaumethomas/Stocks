d000e8af5abc930190536944fa512bad221e971e
Guillaume <guillaumethomas@outlook.com>
2017-06-01 06:57:18 -0700

    flask test

---
+++ project/web_site.py
@@ -0,0 +1,22 @@
+# python 3.6
+from flask import Flask, jsonify
+from portfolio import portfolio
+from stock_calc import total_value, portfolio_string
+
+
+
+app = Flask(__name__)
+
+
+#@app.route('/api/v1.0/tickers')
+@app.route('/')
+def index():
+    a = jsonify(total_value(portfolio()))
+    #a = portfolio_string(portfolio())
+    #print(a)
+    return a
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
